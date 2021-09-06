4c09c5b835eb088b0d17f849b3046425008eb2d6
Abhishek R <abhranja@gmail.com>
2021-09-06 16:51:17 -0700

    B-Tree | Refactor

--- src/com/ar50645/Assignment1.java
+++ src/com/ar50645/Assignment1.java
@@ -8,56 +8,74 @@ import java.util.stream.Stream;
 public class Assignment1 {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //Implement a B-tree with order 3
         BTree<Student> bTree = new BTree<>(3);
 
-        bTree.addElement(new Student(101,"Abhishek",4.0));
-        bTree.addElement(new Student(102,"Anuj",4.0));
-        bTree.addElement(new Student(103,"Hari",4.0));
-        bTree.addElement(new Student(104,"Kshitij",2.8));
-        bTree.addElement(new Student(105,"Shreeraj1",2.8));
-        bTree.addElement(new Student(106, "Adhishek", 3.8));
-        bTree.addElement(new Student(107,"Anujaa",4.0));
-        bTree.addElement(new Student(108,"Dheeraj", 3.9));
-        bTree.addElement(new Student(109,"Shreeraj2", 4.0));
-        bTree.addElement(new Student(110,"Ksitij1",3.998));
-        bTree.addElement(new Student(111,"Ksitij2",3.998));
-        bTree.addElement(new Student(112, "Dhanrj",2.85));
-        bTree.addElement(new Student(113,"Aasit", 2.2));
-        bTree.addElement(new Student(114,"Sanatan",3.43));
-        bTree.addElement(new Student(115,"Gaurav",3.43));
-        bTree.addElement(new Student(116,"Saurav",3.43));
-        bTree.addElement(new Student(117,"Saurabh",3.43));
-        bTree.addElement(new Student(118,"Patil",3.43));
+        //Each element in the B-tree contains a Student object. A Student has a name, redid and GPA
+        Student abhishek = new Student(101,"Abhishek",4.0);
+        Student anuj = new Student(102,"Anuj",4.0);
+        Student kshitij = new Student(104,"Kshitij",3.8);
+        Student hari = new Student(102,"Hari",4.0);
+        Student aashit = new Student(113,"Aasit", 2.2);
+        Student sanatan = new Student(114,"Sanatan",3.43);
+        Student gaurav = new Student(115,"Gaurav",3.43);
+        Student saurav = new Student(116,"Saurav",2.8);
+        Student saurabh = new Student(117,"Saurabh",3.43);
+
+        // Add element in BTree
+        bTree.addElement(abhishek);
+        bTree.addElement(anuj);
+        bTree.addElement(kshitij);
+        bTree.addElement(hari);
+        bTree.addElement(aashit);
+        bTree.addElement(sanatan);
+        bTree.addElement(gaurav);
+        bTree.addElement(saurav);
+        bTree.addElement(saurabh);
 
         System.out.println(bTree);
-//
-        System.out.println("Print out the RedIds of the students that are on probation(GPAlessthan2.85)thatinthe\n" +
+
+        //Given a k,your code returns the kth element in the B-tree in lexicographical order. If k is out-of-bounds throw an exception.
+        bTree.getElement(5);
+//        bTree.getElement(9);
+
+        System.out.println("Print out the RedIds of the students that are on probation(GPA less than 2.85) that in the\n" +
                 "list from the front to the back of the list.");
+
+        printStudentOnProbation(bTree);
+
+        System.out.println("Print out the names of the students with GPA of 4.0 in the list from the back to the front of\n" +
+                "the list");
+
+        printStudentWithGpaInReverseOrder(4.0, bTree);
+
+        System.out.println(bTree.traverse());
+        System.out.println(bTree.traverse().size());
+//        System.out.println(bTree.getElement(17));
+    }
+
+    public static void printStudentOnProbation(BTree<Student> bTree){
         bTree.traverse()
                 .stream()
                 .filter(student -> student.getGpa() < 2.85)
                 .map(student -> student.getName())
                 .forEach(System.out::println);
-//
-        System.out.println("Print out the names of the students with GPA of 4.0 in the list from the back to the front of\n" +
-                "the list");
+    }
+
+    public static void printStudentWithGpaInReverseOrder(double GPA, BTree<Student> bTree){
         reverse(bTree
                 .traverse()
                 .stream())
-            .filter(student -> student.getGpa() == 4.0)
-            .map(student -> student.getName())
-            .forEach(System.out::println);
-
-        System.out.println(bTree.traverse());
-        System.out.println(bTree.traverse().size());
-        System.out.println(bTree.getElement(17));
+                .filter(student -> student.getGpa() == GPA)
+                .map(student -> student.getName())
+                .forEach(System.out::println);
     }
+
     public static <T> Stream<T> reverse(Stream<T> stream)
     {
         LinkedList<T> stack = new LinkedList<>();
         stream.forEach(stack::push);
-
         return stack.stream();
     }
 }
