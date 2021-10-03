fe740512e53a9f4f1410aa598ba5859960bd8f34
Abhishek R <abhranja@gmail.com>
2021-10-03 00:17:59 -0700

    BTree | Add unit tests

--- test/com/aranjan5694/assignment2/TreeSetTest.java
+++ test/com/aranjan5694/assignment2/TreeSetTest.java
@@ -1,30 +1,98 @@
 package com.aranjan5694.assignment2;
 
-import org.junit.jupiter.api.*;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 class TreeSetTest {
 
-    @Test
-    void iterator() {
-    }
+    private static final int ORDER = 3;
 
-    @Test
-    void size() {
-    }
+    // Tree will keep elements in naturally sorted order
+    TreeSet<Integer>  bTree = new TreeSet<>(ORDER);
 
-    @Test
-    void compare() {
+    int[] testData;
+
+    @BeforeEach
+    void setUp() {
+        initializeBtree();
     }
 
     @Test
     void getElement() {
+        Arrays.sort(testData);
+        int expectedValueAtTenthIndex = testData[10];
+        int actualValueAtTenthIndex = bTree.getElement(10);
+        assertEquals(expectedValueAtTenthIndex, actualValueAtTenthIndex);
     }
 
+    //Add Unique Items to BTree and test size of Tree, Order of Elements and Iterator
     @Test
-    void add() {
+    void addElementAndTestSizeAndOrder() {
+
+        Arrays.sort(testData);
+        int[] expectedOrder = testData;
+
+        int[] actualOrder = bTree.stream()
+                .mapToInt(x -> x)
+                .toArray();
+
+        assertEquals(testData.length, bTree.size());
+        Assertions.assertArrayEquals(expectedOrder, actualOrder);
     }
 
+
+    //Duplicate entry shouldn't be allowed as it is set
     @Test
-    void forEach() {
+    void testDuplicateItems() {
+        Assertions.assertTrue(bTree.add(10000));
+        Assertions.assertFalse(bTree.add(10000));
+    }
+
+    //
+    @Test
+    void testForEach() {
+        List<Integer> actualOrder = new ArrayList<>();
+        bTree.forEach(actualOrder::add);
+
+        List<Integer> expectedOrder = reverse(Arrays.stream(testData)
+                .boxed()
+                .sorted())
+                .collect(Collectors.toList());
+
+        assertEquals(actualOrder, expectedOrder);
+    }
+
+
+    @Test
+    void toArray() {
+        Object[] bTreeArray = bTree.toArray();
+        assertEquals(bTreeArray.length, bTree.size());
+    }
+
+    void initializeBtree(){
+        int size = 1000;
+        testData = new int[size];
+        for(int i = 0; i < size; i++) {
+            testData[i] = 1000 - i;
+            assertTrue(bTree.add(testData[i]));
+        }
+
+    }
+
+    public static <T> Stream<T> reverse(Stream<T> stream)
+    {
+        LinkedList<T> stack = new LinkedList<>();
+        stream.forEach(stack::push);
+        return stack.stream();
     }
 }
